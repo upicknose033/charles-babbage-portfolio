240775a7bd22b2d2b6137c16933458c6d4736bc9
PASIAPUATCO1996 <pasiapuatco1996@gmail.com>
2026-01-13 20:12:47 +0800

    Discussion correction

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <NavBar />
   <div class = "container-fluid">
     <About />
     <Projects />
@@ -11,7 +11,7 @@
 
 
 <script setup>
-  import  Navbar from './components/Navbar.vue';
+  import  NavBar from './components/NavBar.vue';
   import About from './components/About.vue';
   import Projects from './components/Projects.vue';
   import Tools from './components/Tools.vue';
